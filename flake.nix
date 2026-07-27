702d2954b118a1b4e4dd37fab34607f51dceb7c1
FloncDev <37045958+FloncDev@users.noreply.github.com>
2026-07-27 03:38:32 +0100

    feat: create flake pacakge

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "beehive development environment";
+  description = "Discord IPC Development Environment";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
@@ -7,21 +7,66 @@
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, utils }:
-    utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs { inherit system overlays; };
 
         rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-          extensions = [ "rust-src" "rust-analyzer" "clippy" ];
+          extensions = [
+            "rust-src"
+            "rust-analyzer"
+            "clippy"
+          ];
         };
 
+        buildDeps = with pkgs; [ pkg-config ];
+
         runtimeDeps = with pkgs; [
-          openssl pkg-config libxkbcommon libGL
-          libX11 libXcursor libXi libXrandr
+          openssl
+          pkg-config
+          libxkbcommon
+          libGL
+          libX11
+          libXcursor
+          libXi
+          libXrandr
         ];
-      in {
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "discord-ipc-bridge";
+          version = "0.1.0";
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuildInputs = buildDeps;
+          buildInputs = runtimeDeps;
+
+          meta = with pkgs.lib; {
+            description = "A bridge for Discord IPC";
+            license = licenses.mit;
+            maintainers = with maintainers; [ flonc ];
+            platforms = platforms.linux;
+          };
+        };
+
+        apps.default = utils.lib.mkApp {
+          drv = self.packages.${system}.default;
+        };
+
         devShells.default = pkgs.mkShell {
           buildInputs = [
             rustToolchain
@@ -30,7 +75,8 @@
             pkgs.nil
             pkgs.nixfmt
             pkgs.nixd
-          ] ++ runtimeDeps;
+          ]
+          ++ runtimeDeps;
 
           shellHook = ''
             export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath runtimeDeps}:$LD_LIBRARY_PATH
